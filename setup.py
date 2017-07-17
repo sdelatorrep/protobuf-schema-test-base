722112a9a53d481c4616b56a760e17e541716e0a
Sabela de la Torre <sabela.delatorre@crg.eu>
2017-07-17 16:49:37 +0200

    Initial commit.

---
+++ setup.py
@@ -0,0 +1,21 @@
+import os
+from setuptools import setup
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(
+    name="protobuf-schema-test-base",
+    version="0.1.0",
+    author="Sabela de la Torre",
+    author_email="sabela.delatorre@crg.eu",
+    description="Base for tests of Protobuf schemas",
+    url="https://github.com/sdelatorrep/protobuf-schema-test-base",
+    license="Apache 2",
+    packages=['tests'],
+    classifiers=[
+        "License :: OSI Approved :: Apache 2 License",
+    ]
+)
