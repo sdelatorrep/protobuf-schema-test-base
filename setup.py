3ce42d61946d2ef874e6b97bc82894350cb71318
Sabela de la Torre <sabela.delatorre@crg.eu>
2017-07-18 13:57:26 +0200

    Add setup info.

--- setup.py
+++ setup.py
@@ -5,7 +5,6 @@ from setuptools import setup
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
-
 setup(
     name="protobuf-schema-test-base",
     version="0.1.0",
@@ -14,8 +13,10 @@ setup(
     description="Base for tests of Protobuf schemas",
     url="https://github.com/sdelatorrep/protobuf-schema-test-base",
     license="Apache 2",
-    packages=['tests'],
+    packages=['tests','ga4gh'],
+    namescape_packages=["ga4gh"]
     classifiers=[
         "License :: OSI Approved :: Apache 2 License",
     ]
 )
+
